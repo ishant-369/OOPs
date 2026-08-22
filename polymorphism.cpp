872d6a2f1ae29e692003bb97fc62553e420f9628
Ishant <isntae@gmail.com>
2026-08-22 18:16:56 +0530

    Basic example of polymorphism using c++
    Added a Student class with parameterized and non-parameterized constructors.

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,1 +1,18 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+class Student{
+public:
+    string name;
+    Student() { 
+        cout<< "non parameterized\n";
+    }
+    Student(string name) {
+        this ->name= name;
+        cout <<"parameterized\n";
+    }
+    };
+int main(){
+    Student s1;
+    return 0 ;
+}
