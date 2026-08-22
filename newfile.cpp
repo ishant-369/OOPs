c595c6daea3bb6ea3c37f26e31f685ad11c9a3be
Ishant <isntae@gmail.com>
2026-08-22 17:35:13 +0530

    code of cpp with opps concept

--- newfile.cpp
+++ newfile.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Person {
+public:
+    string name;
+    int age;
+    //Person(string name, intt age){
+      //  this->name = name;
+      //  this->age-age;
+      Person (){
+        cout << "parent Constructor...\n";
+      }
+    };
 
+class Student : public Person {
+public:
+    int rollno;
+    Student(){
+        cout << "Child constructor...\n";
+    }
+    void getInfo(){
+        cout << "name:"<< name <<endl;
+        cout << "age"<< name <<endl;
+        cout << "rollno:"<< name <<endl;
+    }
+};
+int main() {
+    Student s1;
+    s1.name= "isntae";
+    s1.age= 19;
+    s1.rollno= 119;
+    return 0;
+};
